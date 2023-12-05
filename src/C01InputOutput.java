5e9a17f40824b4824f1d90b855de965204cd3103
GreatJang <1wkdwnsgur@gmail.com>
2023-12-05 12:06:48 +0900

    InputOutput

--- src/C01InputOutput.java
+++ src/C01InputOutput.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 public class C01InputOutput {
     public static void main(String[] args) {
 
-
-//        출력
+//        출력 : System.out
         int a = 20;
 //        print는 출력후 줄바꿈 없음. println은 출력후 줄바꿈
         System.out.print(a);
         String myString = "hello world";
 //        문자열과 숫자를 합하면 문자가 된다.
         System.out.println(myString + a);
+
+//        입력 : System.in(키보드입력) + Scanner(입력을위한클래스)
+        Scanner myScan = new Scanner(System.in);
     }
 }
