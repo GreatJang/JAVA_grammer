bc2be5645d5bf53fec0ef81f3208d3318187c03e
GreatJang <1wkdwnsgur@gmail.com>
2023-12-13 17:48:23 +0900

    231213_commit4

--- src/C11StackQueue.java
+++ src/C11StackQueue.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class C11StackQueue {
     public static void main(String[] args) {
@@ -52,33 +52,141 @@ public class C11StackQueue {
 //            }
 //        }
 
-//        웹사이트 방문 구현
-        Stack<String> backwards = new Stack<>();
-        Stack<String> forwards = new Stack<>();
-        while(true){
-            System.out.println("신규사이트 방문시 1번, 앞으로가기 2번, 뒤로가기 3번");
-            Scanner sc = new Scanner(System.in);
-            String input = sc.nextLine();
-            if(input.equals("1")){
-                System.out.println("방문하신 사이트를 입력해주세요");
-                Scanner sc2 = new Scanner(System.in);
-                String input2 = sc2.nextLine();
-                System.out.println("방문하신 사이트는 " + input2 + "입니다.");
-                backwards.push(input2);
+////        웹사이트 방문 구현
+//        Stack<String> backwards = new Stack<>();
+//        Stack<String> forwards = new Stack<>();
+//        while(true){
+//            System.out.println("신규사이트 방문시 1번, 앞으로가기 2번, 뒤로가기 3번");
+//            Scanner sc = new Scanner(System.in);
+//            String input = sc.nextLine();
+//            if(input.equals("1")){
+//                System.out.println("방문하신 사이트를 입력해주세요");
+//                Scanner sc2 = new Scanner(System.in);
+//                String input2 = sc2.nextLine();
+//                System.out.println("방문하신 사이트는 " + input2 + "입니다.");
+//                backwards.push(input2);
+//
+//            }else if (input.equals("2")){
+//                String temp = forwards.pop();
+//                backwards.push(temp);
+//                System.out.println("앞으로 가기를 통해 방문한 곳은 " + temp +"입니다.");
+//
+//
+//            }else{
+//                String temp = backwards.pop();
+//                forwards.push(temp);
+//                System.out.println("뒤로가기로 방문하신 곳은 " + backwards.peek() + "입니다.");
+//
+//            }
+//        }
 
-            }else if (input.equals("2")){
-                String temp = forwards.pop();
-                backwards.push(temp);
-                System.out.println("앞으로 가기를 통해 방문한 곳은 " + temp +"입니다.");
+////        큐선언
+//        Queue<Integer> myQue = new LinkedList<>();
+//        myQue.add(10);
+//        myQue.add(20);
+//        myQue.add(30);
+//        System.out.println(myQue.poll());
+//        System.out.println(myQue);
+
+////        프린터대기열 예제
+////        문서1추가, 문서2추가, 문서3추가
+////        while문을 통해 "현재 인쇄중인 문서 : "
+//        Queue<String> myQQ = new LinkedList<>();
+//        myQQ.add("문서1");
+//        myQQ.add("문서2");
+//        myQQ.add("문서3");
+//        while(!myQQ.isEmpty()){
+//            System.out.println("현재 인쇄중인 문서 : " + myQQ.poll());
+//        }
+
+////        길이의 제한이 있는 큐 : ArrayBlockingQueue
+//        Queue<String> myQue = new ArrayBlockingQueue<>(3);
+////        add와 offer의 차이 : add는 길이가 다 찼을 때 에러를 발생(IllegalStateException: Queue full). offer는 공간이 충분할 때만 add
+//        myQue.offer("hello1");
+//        myQue.offer("hello2");
+//        myQue.offer("hello3");
+//        myQue.offer("hello4"); // offer 시 hello4번은 공긴이 없어서 안들어감.
+//        System.out.println(myQue);
 
 
-            }else{
-                String temp = backwards.pop();
-                forwards.push(temp);
-                System.out.println("뒤로가기로 방문하신 곳은 " + backwards.peek() + "입니다.");
+//        ⭐우선순위 큐
 
-            }
+//        Queue<Integer> pq = new PriorityQueue<>();
+//        pq.add(30);
+//        pq.add(20);
+//        pq.add(10);
+//        pq.add(40);
+//        pq.add(50);
+//        System.out.println(pq);
+////        내부적으로 힙 자료구조를 가지고 있기 때문에 정렬이 깔끔하지 않다. 원칙적으로는 맨 앞에 제일 작은 값이 있다.
+//        while(!pq.isEmpty()){
+//            System.out.println(pq.poll());
+//        }
+//
+
+
+
+////        ⭐프로그래머스 더 맵게 문제
+        int[] scoville = {1,2,3,9,10,12};
+        int K=7;
+//
+//        list로 변환 -> int temp = get(0) + get(1)*2;
+//        remove(0), remove(0)
+
+//        List로 문제 풀이 시 시간 효울성 에러
+        List<Integer> myList  = new ArrayList<>();
+        for(int a : scoville){
+            myList.add(a);
         }
+        int answer = 0;
+                while(true){
+                    if(myList.get(0) >= K){
+                        break;
+                    }else if(myList.size() == 1 && myList.get(0)<K){
+                        answer = -1;
+                        break;
+                    }else{
+                        answer++;
+                        int temp = myList.get(0) + myList.get(1)*2;
+                        myList.remove(0);
+                        myList.remove(0);
+                        myList.add(temp);
+                        Collections.sort(myList);
+                    }
+
+                }
+                System.out.println(answer);
+
+
+////        내 풀이
+//        int answer = 0;
+//        Queue<Integer> myQue = new PriorityQueue<>();
+//        for(int a : scoville){
+//            myQue.add(a);
+//        }
+//        for (int i = 0; i <= K; i++) {
+//            if(myQue.size() == 1 && myQue.peek()<K){
+//                answer = -1;
+//                break;
+//            }
+//            if(myQue.peek()>=K){
+//                break;
+//            }
+//            int temp = myQue.poll();
+//            int temp1 = myQue.poll();
+//            int temp2 = temp + (temp1*2);
+//            myQue.add(temp2);
+//            answer ++;
+//
+//        }
+//        System.out.println(answer);
+
+
+
+
+
+
+
 
 
 
--- src/test2.java
+++ src/test2.java
@@ -5,18 +5,54 @@ import java.util.*;
 
 public class test2 {
     public static void main(String[] args) {
+        int[] scoville = {1,2,3,9,10,12};
+        int K=7;
+
+        int answer = 0;
+        Queue<Integer> myQue = new PriorityQueue<>();
+        for(int a : scoville){
+            myQue.add(a);
+        }
+        for (int i = 0; i <= K; i++) {
+            if(myQue.size() == 1 && myQue.peek()<K){
+                break;
+            }
+            if(myQue.peek()>=K){
+                break;
+            }
+            int temp = myQue.poll();
+            int temp1 = myQue.poll();
+            int temp2 = temp + (temp1*2);
+            myQue.add(temp2);
+            answer ++;
+
+        }
+        System.out.println(answer);
 
 
+//        int temp = myQue.poll();
+//        int temp1 = myQue.poll();
+//        System.out.println(myQue);
+//        int temp2 = temp + (temp1*2);
+//        myQue.add(temp2);
+//        System.out.println(myQue);
 
-//        String my_string = "hello";
-//        int num1 = 1;
-//        int num2 = 2;
-//        String[] Arr = new String[my_string.length()];
-//        String temp = Arr[num1];
-//        Arr[num1] = Arr[num2];
-//        Arr[num2] =
-//
-//            String answer = "";
+//        Queue<Integer> pq = new PriorityQueue<>();
+//        pq.add(30);
+//        pq.add(20);
+//        pq.add(10);
+//        pq.add(40);
+//        pq.add(50);
+//        System.out.println(pq);
+////        내부적으로 힙 자료구조를 가지고 있기 때문에 정렬이 깔끔하지 않다. 원칙적으로는 맨 앞에 제일 작은 값이 있다.
+//        while(!pq.isEmpty()){
+//            System.out.println(pq.poll());
+//        myQQ.add("문서1");
+//        myQQ.add("문서2");
+//        myQQ.add("문서3");
+//        while(!myQQ.isEmpty()){
+//            System.out.println("현재 인쇄중인 문서 : " + myQQ.poll());
+//        }
 
 
     }
