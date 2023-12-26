07e2d9da546af221a917e760c1e69af978480f73
GreatJang <1wkdwnsgur@gmail.com>
2023-12-26 10:37:51 +0900

    231226_commit1

--- src/C16EtcClass/C1602Calendar.java
+++ src/C16EtcClass/C1602Calendar.java
@@ -3,10 +3,11 @@ package C16EtcClass;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 
 public class C1602Calendar {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 ////        날짜 관련 클래스의 종류 : Calendar(java.util), java.time패키지 안에 있는 Local~ 클래스.
 ////        주로 java.time패키지 안에 있는 Local~ 클래스 사용 // LocalDateTime
 //        Calendar time = Calendar.getInstance(); // 현재시간 알려주는 객체
@@ -31,5 +32,31 @@ public class C1602Calendar {
 
         LocalDateTime this_time = LocalDateTime.now();
         System.out.println(this_time);
+
+//        임의로 특정시간을 만들어 내고 싶을때 : of 메서드 사용
+        LocalDate birthDay = LocalDate.of(1980,02,19);
+//        일반 내장 메서드
+        System.out.println(birthDay.getYear());
+        System.out.println(birthDay.getMonth());
+        System.out.println(birthDay.getDayOfMonth());
+        LocalTime birthTime = LocalTime.of(04,02,19);
+
+        LocalDateTime birthDayTime = LocalDateTime.of(birthDay,birthTime);
+
+//        크로노필드 enum 타입 사용 : 매개변수로 크로노필드를 받아 프로그램의 유연성 향상
+        System.out.println(birthDayTime.get(ChronoField.YEAR));
+        System.out.println(birthDayTime.get(ChronoField.MONTH_OF_YEAR));
+//        0 : 오전, 1 : 오후
+        System.out.println(birthDayTime.get(ChronoField.AMPM_OF_DAY));
+
+//        현재시간은 오전/오후 입니다.
+        if(birthDayTime.get(ChronoField.AMPM_OF_DAY) == 0){
+            System.out.println("현재 시간은 오전 입니다.");
+        }else{
+            System.out.println("현재 시간은 오후 입니다.");
+        }
+
+
+
     }
 }
--- src/test.java
+++ src/test.java
@@ -5,14 +5,17 @@ public class test {
 
         int[] num_list = {100, 95, 2, 4, 5, 6, 18, 33, 948};
         int n = 3;
-        int[][] answer = {};
+        int[][] answer = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int a = 0; a < num_list.length; a++) {
+                for (int j = 0; j < n; j++) {
+                    answer[i][j] = num_list[a];
+                }
+            }
+        }
 
-
-
-
-
+        System.out.println(Arrays.deepToString(answer));
 //            return answer;
-
 
 
     }
