072616938cf58a63c81daba52b7dca493734e11d
GreatJang <1wkdwnsgur@gmail.com>
2023-12-26 17:57:42 +0900

    231226_commit5

---
+++ src/C17ExceptionFileParsing/AuthorException/AuthorRepository.java
@@ -0,0 +1,24 @@
+package C17ExceptionFileParsing.AuthorException;
+
+import java.util.*;
+class AuthorRepository {
+    List<Author> authors;
+    AuthorRepository(){
+        authors = new ArrayList<>();
+    }
+    void register(Author author){
+        authors.add(author);
+    }
+    List<Author> getAuthors(){
+        return authors;
+    }
+
+    Optional<Author> getAuthorByEmail(String email){
+        for(Author a : this.authors){
+            if(a.getEmail().equals(email)){
+                return Optional.of(a);
+            }
+        }
+        return Optional.empty();
+    }
+}
