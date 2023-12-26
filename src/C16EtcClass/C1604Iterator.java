5217e65826d2662fb7d4fd2e1a07cf900c89ae93
GreatJang <1wkdwnsgur@gmail.com>
2023-12-26 12:56:04 +0900

    231226_commit3

---
+++ src/C16EtcClass/C1604Iterator.java
@@ -0,0 +1,31 @@
+package C16EtcClass;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class C1604Iterator {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+            myList.add("apple");
+            myList.add("banana");
+            myList.add("cherry");
+
+//            enhanced for문 : 원본이 변경X -> remove 불가
+        for(String a : myList){
+            System.out.println(a);
+//            remove메서드 없음
+        }
+
+//        iterator사용 : 참조하고 있는 원본데이터 삭제 가능 -> remove
+        Iterator<String> myIter = myList.iterator();
+        while(myIter.hasNext()){
+            if(myIter.next().equals("banana")){
+                myIter.remove();
+            }
+        }
+        System.out.println(myList);
+
+
+    }
+}
