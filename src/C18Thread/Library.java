6635d0361604bf85cc8ec0e95ab890434e8ea253
GreatJang <1wkdwnsgur@gmail.com>
2023-12-27 17:12:09 +0900

    231227_commit6

---
+++ src/C18Thread/Library.java
@@ -0,0 +1,21 @@
+package C18Thread;
+
+public class Library {
+    static int bookcount = 100;
+
+    public static void borrowBook() {
+        if (bookcount > 0) {
+//            책을 빌리는 시간을 10밀리초로 가정.
+            try {
+                Thread.sleep(10); // 10밀리초 쉼
+            } catch (InterruptedException e) { // checked exception
+                System.out.println(e.getMessage()); // RuntimeException : unchecked exception
+            }
+            bookcount -= 1;
+            System.out.println("대출완료");
+            System.out.println("남아있는 책수량" + bookcount);
+        } else {
+            System.out.println("대출불가");
+        }
+    }
+}
