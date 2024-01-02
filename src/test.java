d22b4a3f5dc2a25b10a8358da0d70f771992ec1d
GreatJang <1wkdwnsgur@gmail.com>
2024-01-02 17:54:28 +0900

    algorithm_study_good

--- src/test.java
+++ src/test.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class test {
-    public int solution(int n) {
-        n = 7; //3! 3출력
-//        i! ≤ n
-        int answer = 1;
-        for (int i = 2; i <= 10; i++) {
-            answer = answer * i;
-            if(answer ==n){
-                return i;
-            }
-            if(answer > n){
-                return --i;
+    public static void main(String[] args) {
+        String before = "oleeh";
+        String after = "hello";
+        StringBuilder aft = new StringBuilder(after);
+        int answer;
+        for (int i = 0; i < before.length(); i++) {
+            String a = String.valueOf(before.charAt(i));
+            if (after.contains(a)) {
+                aft.deleteCharAt(Integer.parseInt(a));
+                System.out.println(aft);
+
+//                StringBuilder MyString = new StringBuilder("Hello World");
+//                System.out.println("The string before removing character: " + MyString);
+//                MyString = MyString.deleteCharAt(5);
+//                System.out.println(after);
             }
         }
-        return answer;
+//        if (count == after.length()) {
+//            answer = 1;
+//        } else {
+//            answer = 0;
+//        }
+//        System.out.println(answer);
+
     }
 }
