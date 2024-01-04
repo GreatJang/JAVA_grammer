2d9ac8697d55856d449bf6456a7e7b9cde7b5359
unknown <1wkdwnsgur@gmail.com>
2024-01-04 23:37:40 +0900

    coding_test_practice

--- src/test.java
+++ src/test.java
@@ -1,65 +1,65 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-class test {
-<<<<<<< HEAD
-    static int[][] static_computers;
-    public static void main(String[] args) {
-        int n = 3;
-        int[][] computers ={{1,1,0},{1,1,1},{0,1,1}};
-        static_computers = computers;
-        dfs(0);
-
-
-
-
-    }
-    static void dfs(int start){
-        System.out.println(start);
-        int count=0;
-        for(int i=0; i< static_computers.length; i++){
-            if(static_computers[start][i]==1){
-                dfs(i);
-            }
-        }
-    }
-=======
-    public static void main(String[] args) {
-        String before = "olleh";
-        String after = "hello";
-
-        StringBuilder aft = new StringBuilder(after);
-        for (int i = 0; i < before.length(); i++) {
-            String a = String.valueOf(before.charAt(i));
-
-//            System.out.println(aft.indexOf(a));
-            if(aft.indexOf(a) == -1){
-                System.out.println(0);
-                break;
-            }
-            if(aft.indexOf(a)==0){
-                System.out.println(1);
-            }
-            aft.deleteCharAt(aft.indexOf(a));
-//            System.out.println(aft.indexOf(a));
-//            if (aft.(a)) {
-//                aft.deleteCharAt(after.indexOf(a));
-//                System.out.println(aft);
-
-//                StringBuilder MyString = new StringBuilder("Hello World");
-//                System.out.println("The string before removing character: " + MyString);
-//                MyString = MyString.deleteCharAt(5);
-//                System.out.println(after);
-            }
-        }
-//        if (count == after.length()) {
-//            answer = 1;
-//        } else {
-//            answer = 0;
-//        }
-//        System.out.println(answer);
-
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//class test {
+//<<<<<<< HEAD
+//    static int[][] static_computers;
+//    public static void main(String[] args) {
+//        int n = 3;
+//        int[][] computers ={{1,1,0},{1,1,1},{0,1,1}};
+//        static_computers = computers;
+//        dfs(0);
+//
+//
+//
+//
 //    }
->>>>>>> 897743c219b9344b7ebc5916653b689c02b6504e
-}
+//    static void dfs(int start){
+//        System.out.println(start);
+//        int count=0;
+//        for(int i=0; i< static_computers.length; i++){
+//            if(static_computers[start][i]==1){
+//                dfs(i);
+//            }
+//        }
+//    }
+//=======
+//    public static void main(String[] args) {
+//        String before = "olleh";
+//        String after = "hello";
+//
+//        StringBuilder aft = new StringBuilder(after);
+//        for (int i = 0; i < before.length(); i++) {
+//            String a = String.valueOf(before.charAt(i));
+//
+////            System.out.println(aft.indexOf(a));
+//            if(aft.indexOf(a) == -1){
+//                System.out.println(0);
+//                break;
+//            }
+//            if(aft.indexOf(a)==0){
+//                System.out.println(1);
+//            }
+//            aft.deleteCharAt(aft.indexOf(a));
+////            System.out.println(aft.indexOf(a));
+////            if (aft.(a)) {
+////                aft.deleteCharAt(after.indexOf(a));
+////                System.out.println(aft);
+//
+////                StringBuilder MyString = new StringBuilder("Hello World");
+////                System.out.println("The string before removing character: " + MyString);
+////                MyString = MyString.deleteCharAt(5);
+////                System.out.println(after);
+//            }
+//        }
+////        if (count == after.length()) {
+////            answer = 1;
+////        } else {
+////            answer = 0;
+////        }
+////        System.out.println(answer);
+//
+////    }
+//>>>>>>> 897743c219b9344b7ebc5916653b689c02b6504e
+//}
--- src/test3.java
+++ src/test3.java
@@ -3,9 +3,7 @@ import C14Interface.Bank.Model.BankAccount;
 import java.util.*;
 class test3 {
     public static void main(String[] args) {
-
-        int a = 11;
-
-        System.out.println();
+        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
+        Arrays.sort(num_list);
     }
 }
