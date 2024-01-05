b84e68c6dc1f5e95b6f2685176824e202130f6ab
unknown <1wkdwnsgur@gmail.com>
2024-01-06 00:23:36 +0900

    coding_test_practice

--- src/test3.java
+++ src/test3.java
@@ -3,7 +3,17 @@ import C14Interface.Bank.Model.BankAccount;
 import java.util.*;
 class test3 {
     public static void main(String[] args) {
-        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
-        Arrays.sort(num_list);
+
+        int[] arr = {1, 2, 3, 100, 99, 98};
+        int[] answer = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] >= 50 && arr[i]%2==0){
+                answer[i] = arr[i]/2;
+            }
+            if(arr[i] < 50 && arr[i]%2==1){
+                answer[i] = arr[i]*2;
+            }
+        }
+        System.out.println(answer);
     }
 }
