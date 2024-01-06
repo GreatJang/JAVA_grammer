8f589dcb1e7acf37de1978df382b348bad8d6c2d
GreatJang <1wkdwnsgur@gmail.com>
2024-01-06 17:37:05 +0900

    algorithm_study_good

--- src/test3.java
+++ src/test3.java
@@ -3,7 +3,7 @@ import C14Interface.Bank.Model.BankAccount;
 import java.util.*;
 class test3 {
     public static void main(String[] args) {
-        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
-        Arrays.sort(num_list);
+        String my_string = "banana";
+        my_string.contains()
     }
 }
