6020ba33decceed2769a5cd9f532c0ba3cf97061
GreatJang <1wkdwnsgur@gmail.com>
2024-01-08 19:25:30 +0900

    test3_update

--- src/test3.java
+++ src/test3.java
@@ -3,5 +3,19 @@ import C14Interface.Bank.Model.BankAccount;
 import java.util.*;
 class test3 {
     public static void main(String[] args) {
+
+        String my_string = "banana";
+        String is_prefix = "ban";
+            if(my_string.length()<is_prefix.length()){
+                System.out.println(0);
+            }
+            String a = my_string.substring(0,is_prefix.length());
+            if(a.equals(is_prefix)){
+                System.out.println(1);
+            }
+            else {
+                System.out.println(0);
+            }
+
     }
 }
